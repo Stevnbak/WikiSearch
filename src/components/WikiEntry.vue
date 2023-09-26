6efbf47723b82d7af27c2f3078858baeac59f5fc
Stevnbak <kasper@stevnbak.dk>
2023-09-26 13:46:21 +0200

    Collapsible categories

--- src/components/WikiEntry.vue
+++ src/components/WikiEntry.vue
@@ -1,27 +1,49 @@
 <template>
 	<a class="entry" :href="info.homepage" target="_blank">
 		<h2>{{ info.name }}</h2>
-		<p>URL: {{ info.homepage }}</p>
-		<p>Language: {{ info.lang }}</p>
-		<div v-if="info.games" class="category games">
-			<h3>Games</h3>
-			<li v-for="game in info.games">{{ game }}</li>
+		<div class="info">
+			<div v-if="info.api" class="logo">
+				{{ getWikiLogo(info.api) }}
+				<img :src="imageUrls[info.api]" />
+			</div>
+			<p>URL: {{ info.homepage }}</p>
+			<p>Language: {{ info.lang }}</p>
 		</div>
-		<div v-if="info.series" class="category series">
-			<h3>Series</h3>
-			<li v-for="series in info.series">{{ series }}</li>
-		</div>
-		<div v-if="info.companies" class="category companies">
-			<h3>Companies</h3>
-			<li v-for="company in info.companies">{{ company }}</li>
-		</div>
-		<div v-if="info.genres" class="category genres">
-			<h3>Genres</h3>
-			<li v-for="genres in info.genres">{{ genres }}</li>
-		</div>
-		<div v-if="info.api" class="logo">
-			{{ getWikiLogo(info.api) }}
-			<img :src="imageUrls[info.api]" onerror="this.style.display='none'" />
+		<div class="categories">
+			<div v-if="info.games" class="category">
+				<div>
+					<h3>Games</h3>
+					<button @click.prevent="changeCollapse('games')">{{ gamesCollapsed ? "+" : "-" }}</button>
+				</div>
+				<ul :style="'display: ' + (gamesCollapsed ? 'none' : 'block')">
+					<li v-for="game in info.games">{{ game }}</li>
+				</ul>
+			</div>
+			<div v-if="info.series" class="category">
+				<div>
+					<h3>Series</h3>
+					<button @click.prevent="changeCollapse('series')">{{ seriesCollapsed ? "+" : "-" }}</button>
+				</div>
+				<ul :style="'display: ' + (seriesCollapsed ? 'none' : 'block')">
+					<li v-for="series in info.series">{{ series }}</li>
+				</ul>
+			</div>
+			<div v-if="info.companies" class="category">
+				<div>
+					<h3>Companies</h3>
+					<button @click.prevent="changeCollapse('companies')">{{ companiesCollapsed ? "+" : "-" }}</button>
+				</div>
+				<ul :style="'display: ' + (companiesCollapsed ? 'none' : 'block')">
+					<li v-for="company in info.companies">{{ company }}</li>
+				</ul>
+			</div>
+			<div v-if="info.genres" class="category">
+				<h3>Genres</h3>
+				<button @click.prevent="changeCollapse('genres')">{{ genresCollapsed ? "+" : "-" }}</button>
+				<ul :style="'display: ' + (genresCollapsed ? 'none' : 'block')">
+					<li v-for="genres in info.genres">{{ genres }}</li>
+				</ul>
+			</div>
 		</div>
 	</a>
 </template>
@@ -31,16 +53,18 @@
 	import type {wikiData} from "@/main";
 	import type {PropType} from "vue";
 	export default defineComponent({
+		data: () => ({
+			imageUrls: {} as any,
+			gamesCollapsed: true,
+			companiesCollapsed: true,
+			seriesCollapsed: true,
+			genresCollapsed: true
+		}),
 		props: {
 			info: {
 				type: Object as PropType<wikiData>,
 				required: true
 			}
-		},
-		data() {
-			return {
-				imageUrls: {} as any
-			};
 		},
 		methods: {
 			getWikiLogo(url: string): void {
@@ -59,7 +83,26 @@
 							this.imageUrls[url] = image;
 						}
 					});
+			},
+			changeCollapse(category: string) {
+				switch (category) {
+					case "games":
+						this.gamesCollapsed = !this.gamesCollapsed;
+						break;
+					case "series":
+						this.seriesCollapsed = !this.seriesCollapsed;
+						break;
+					case "companies":
+						this.companiesCollapsed = !this.companiesCollapsed;
+						break;
+					case "genres":
+						this.genresCollapsed = !this.genresCollapsed;
+						break;
+				}
 			}
+		},
+		mounted() {
+			console.log("Test");
 		}
 	});
 </script>
@@ -90,7 +133,6 @@
 	h3,
 	p,
 	li {
-		width: 100%;
 		text-decoration: none;
 		color: var(--color-text);
 	}
@@ -105,11 +147,44 @@
 		color: var(--color-heading);
 		font-weight: bold;
 	}
+
+	.info {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		flex-wrap: wrap;
+	}
+	.categories {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 	.category {
 		width: 100%;
 		display: flex;
 		flex-direction: column;
 		justify-content: flex-start;
+		align-items: center;
+	}
+	.category div {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+	}
+	.category button {
+		height: 1.5rem;
+		aspect-ratio: 1;
+	}
+	.category ul {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
 		align-items: flex-start;
 	}
 
